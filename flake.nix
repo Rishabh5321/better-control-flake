5886b35cd8a393956841eccba1bfa3159ae86476
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-24 20:00:23 +0530

    Update GitHub revision

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
           src = pkgs.fetchFromGitHub {
             owner = "quantumvoid0";
             repo = "better-control";
-            rev = "ff270596815a7da7d876dec7dac11ee2ce566e33";
+            rev = "f13a49e2126db44476938fbb633047a5d360b79d";
             sha256 = "sha256-MJ+YfC+NfCPojb3HT2PmWgr0BWpQGkXLpVHkIP8plJY=";
           };
 
