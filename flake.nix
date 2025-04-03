c334b2e165c170bc93f41e5e6ca90f28b8c15611
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-04-03 10:13:57 +0530

    refactor: restructure better-control package and remove obsolete desktop entry

--- flake.nix
+++ flake.nix
@@ -1,114 +1,31 @@
 {
-  description = "better-control - A system control panel utility";
+  description = "Better Control - A system control panel utility";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-      version = "5.8";
-    in
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        better-control = pkgs.callPackage ./package.nix {};
       in
-      rec {
-        packages.default = packages.better-control;
-        packages.better-control = pkgs.stdenv.mkDerivation {
-          pname = "better-control";
-          inherit version;
-          src = pkgs.fetchzip {
-            url = "https://github.com/quantumvoid0/better-control/archive/refs/tags/${version}.zip";
-            sha256 = "1kijpnkyjvvjyvkk30h0x6n37jr77w14gi5ccasvcj0vlvngdm0m";
-          };
-          buildInputs = with pkgs; [
-            gtk3
-            networkmanager
-            bluez
-            pipewire
-            brightnessctl
-            python3
-            python3Packages.pygobject3
-            python3Packages.dbus-python
-            python3Packages.pydbus
-            python3Packages.psutil
-            power-profiles-daemon
-            python3Packages.qrcode
-            python3Packages.requests
-            python3Packages.pillow # For QR code image handling
-            python3Packages.pycairo # For QR code generation
-            gammastep
-            libpulseaudio
-            pulseaudio
-          ];
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            wrapGAppsHook
-            python3Packages.wrapPython
-            makeWrapper
-            desktop-file-utils # For desktop file validation
-          ];
-          dontBuild = true;
-          sourceRoot = "source/";
-          installPhase = ''
-            mkdir -p $out/bin $out/share/better-control $out/share/applications
-
-            # Copy application files
-            cp -r src/* $out/share/better-control/
-
-            # Create better-control executable
-            cat > $out/bin/better-control << EOF
-            #!/bin/sh
-            exec python3 $out/share/better-control/better_control.py "\$@"
-            EOF
-            chmod +x $out/bin/better-control
-
-            # Install desktop file from the root directory
-            cp ${./control.desktop} $out/share/applications/better-control.desktop
-            substituteInPlace $out/share/applications/better-control.desktop \
-              --replace 'Exec=better-control' "Exec=$out/bin/better-control"
-          '';
-          postFixup = ''
-            wrapPythonPrograms
-            wrapProgram $out/bin/better-control \
-              --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [
-                python3
-                brightnessctl
-                networkmanager
-                bluez
-                pipewire
-                power-profiles-daemon
-                gammastep
-                libpulseaudio
-                pulseaudio
-              ])} \
-              --prefix GI_TYPELIB_PATH : "${pkgs.lib.makeSearchPath "lib/girepository-1.0" [
-                pkgs.gtk3
-                pkgs.gobject-introspection
-                pkgs.pango
-              ]}" \
-              --set PYTHONPATH "$PYTHONPATH:${pkgs.python3Packages.pygobject3}/${pkgs.python3.sitePackages}" \
-              --set DBUS_SYSTEM_BUS_ADDRESS "unix:path=/run/dbus/system_bus_socket" 
-          '';
-          meta = with pkgs.lib; {
-            description = "A system control panel utility";
-            homepage = "https://github.com/quantumvoid0/better-control";
-            license = licenses.gpl3Only;
-            platforms = platforms.linux;
-            mainProgram = "better-control";
-            maintainers = [ maintainers.quantumvoid maintainers.nekrooo ];
-          };
+      {
+        packages = {
+          default = better-control;
+          better-control = better-control;
         };
-        nixosModules.default = { config, lib, ... }: {
-          config = lib.mkIf (config.services.power-profiles-daemon.enable or false) {
-            services.power-profiles-daemon.enable = true;
-            services.dbus.packages = [ self.packages.${system}.better-control ];
-            environment.systemPackages = [ self.packages.${system}.better-control ];
-          };
+
+        apps.default = {
+          type = "app";
+          program = "${better-control}/bin/better-control";
         };
+
         devShells.default = pkgs.mkShell {
-          buildInputs = self.packages.${system}.better-control.buildInputs ++ [ pkgs.gnumake ];
+          buildInputs = better-control.buildInputs;
+          nativeBuildInputs = better-control.nativeBuildInputs;
         };
-      }
-    );
+      });
 }
