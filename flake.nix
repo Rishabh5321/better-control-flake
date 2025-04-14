342b0ed6ad4689077728ebf4ce44734fe40b720e
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-04-14 12:31:49 +0530

    fix: update nixpkgs reference from release-24.11 to nixos-unstable in flake files

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Better Control - A system control panel utility";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
